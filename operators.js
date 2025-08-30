ecfaa39a42dcbb25c3e6c0d7c9bbeb50b1933e45
MwaranoKelvin <kelvinmwarano4@gmail.com>
2025-08-30 14:07:37 +0300

    Initial Commit

---
+++ operators.js
@@ -0,0 +1,24 @@
+let a = 10, b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+
+//Assignments Operators
+let x = 10
+x += 5
+console.log(x);
+x *= 2;
+console.log(x);
+
+//Comparison Operators
+/*
+ == Equal(value only) e.g. 5 == "5", Is true 
+ === Strictly equal (value & type) e.g. 5 === "5", is False
+ != Not equal 5 != 4, True
+ !== Strictly not equal to, 5 !== "5", True
+ > greater than
+ < less than
+ >= Greater than or equal to
+ <= Less than or equal to
+*/
+
